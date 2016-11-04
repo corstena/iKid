694293dcad7ec0f6d6edcd589b288b99c74c998a
Albert Corsten <corstena@uw.edu>
2016-11-04 16:35:47 -0700

    Complete

--- iKid/firstJokeViewController.swift
+++ iKid/firstJokeViewController.swift
@@ -39,6 +39,7 @@ class firstJokeViewController: UIViewController {
                                 self.punchline = true
             })
         } else {
+            //Comment
             UIView.transition(with: goodJoke1,
                               duration: 0.5,
                               options: UIViewAnimationOptions.transitionFlipFromLeft,
